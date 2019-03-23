6db6ea32458a086a6607d9906466507f2863f6f9
JL Granda <jorgelugra@MacBook-Pro-de-JL.local>
2019-03-22 20:49:26 -0500

    doc updated

--- rekognition_apis.py
+++ rekognition_apis.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv  # DotEnv
 import rtsp  # Cam Protoccol for StreamReader
 from os import getenv  # Get environment variale
 
+
 class RekognitionApis():
 
     def __init__(self):
@@ -10,20 +11,39 @@ class RekognitionApis():
         """
         load_dotenv()  # Load env variables
         self.client = boto3.client(getenv('CLIENT'))
+        # AWS Collection to store metadata
         self.collectionId = getenv('COLLECTION')
+        # S3 bucket for indexing the images
         self.bucket = getenv('BUCKET')
+        # Minimun percentaje to pass
         self.threshold = 99
+        # Amount of faces in API results
         self.maxFaces = 1
 
-    def create_collection(self):
+    def create_collection(self, name):
+        """Create a collection on AWS Rekognition to store metadata
+        Args:
+            name (str): Name for the collection
+
+        Returns:
+            None
+        """
         print('Creating collection:' + self.collectionId)
         response = self.client.create_collection(
             CollectionId=self.collectionId)
+        # Amazon Resource Name
         print('Collection ARN: ' + response['CollectionArn'])
+        # Http response code
         print('Status code: ' + str(response['StatusCode']))
         print('Done...')
 
     def list_faces(self):
+        """ List faces on the collection.
+        Validate if there is more than one result on the response array otherwise,
+        breaks the loop.
+
+        """
+        # To avoid infinity loop
         token = True
         maxResults = 1
         response = self.client.list_faces(CollectionId=self.collectionId)
@@ -53,12 +73,13 @@ class RekognitionApis():
         # TODO Validate name format (name_lastname_X.jpg)
         response = self.client.index_faces
         for secuence in range(from_n, to_n+1):
+            # Format the string as Name_LastName_X.jpg
             name_secuence = '{}_{}.jpg'.format(name, secuence)
             response = self.client.index_faces(CollectionId=self.collectionId,
                                                Image={'S3Object': {
                                                    'Bucket': self.bucket, 'Name': name_secuence}},
                                                ExternalImageId=name_secuence,
-                                               MaxFaces=1,
+                                               MaxFaces=self.maxFaces,
                                                QualityFilter="AUTO",
                                                DetectionAttributes=['ALL'])
 
@@ -69,6 +90,7 @@ class RekognitionApis():
                 print('  Location: {}'.format(
                     faceRecord['Face']['BoundingBox']))
 
+            # Get the faces with Indexing problems
             print('Faces not indexed:')
             for unindexedFace in response['UnindexedFaces']:
                 print(' Location: {}'.format(
@@ -78,6 +100,10 @@ class RekognitionApis():
                     print('   ' + reason)
 
     def search_by_id(self, photoId):
+        """Get the similarity score comparing the ID with the existing ones in the collection
+        Args:
+            photoId (str): ID in format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+        """
         response = self.client.search_faces(CollectionId=self.collectionId,
                                             FaceId=photoId,
                                             FaceMatchThreshold=self.threshold,
@@ -90,8 +116,18 @@ class RekognitionApis():
             print('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
             print()
 
-    def search_by_image(self, fileName, mode='local'):
-        imageFile = 'input.jpg'
+    def search_by_image(self, imageFile='input.jpg', mode='local'):
+        """Gets the similarity score sending the image in ByteArray.
+        If the local parameter is specified it gets the imagen from the same folder
+        of the sccript, otherwise it gets the image from am AWS S3 bucket.
+
+        Args:
+            fileName (str): 
+            mode (str): Local image or S3 image.
+        
+        Returns:
+            None
+        """
         if mode == 'local':
             with open(imageFile, 'rb') as image:
                 response = self.client.search_faces_by_image(CollectionId=self.collectionId,
@@ -99,7 +135,9 @@ class RekognitionApis():
                                                                  'Bytes': image.read()},
                                                              FaceMatchThreshold=self.threshold,
                                                              MaxFaces=self.maxFaces)
-        elif mode =='s3':
+        elif mode == 's3':
+            #TODO Get the name of the image on the bucket
+            fileName = '' 
             response = self.client.search_faces_by_image(CollectionId=self.collectionId,
                                                          Image={'S3Object': {
                                                              'Bucket': self.bucket, 'Name': fileName}},
