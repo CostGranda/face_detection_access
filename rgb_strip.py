466de47a0efda03a252f86f84b57b41b2535719e
JL Granda <jorgelugra@MacBook-Pro-de-JL.local>
2019-05-13 18:34:28 -0500

    strip class complete

--- rgb_strip.py
+++ rgb_strip.py
@@ -1,10 +1,54 @@
-import pigpio
-from time import sleep
-R=17
-G=22
-B=24
-pi = pigpio.pi()
-pi.set_PWM_dutycycle(R, 255)
-sleep(10)
-pi.set_PWM_dutycycle(R, 0)
-pi.stop()
+try:
+    import pigpio
+except RuntimeError as E:
+    print("Cannot load the pigpio module")
+
+class RGB_Strip():
+
+    def __init__(self, r, g, b):
+        """
+        Receives the GPIO Pin's to manipulate
+        """
+        self.R = r #RED LEDS
+        self.G = g #GREEN LEDS
+        self.B = b #BLUE LEDS
+        # Maximum Brightnes
+        self.ON = 255
+        # Minimum Brightnes
+        self.OFF = 0
+        # Neecsita el demonio inicicalizado (pigpiod)
+        self.pi = pigpio.pi()
+
+    def red_on(self):
+        self.pi.set_PWM_dutycycle(self.R, self.ON)
+
+    def green_on(self):
+        self.pi.set_PWM_dutycycle(self.G, self.ON)
+
+    def blue_on(self):
+        self.pi.set_PWM_dutycycle(self.B, self.ON)
+
+    def red_off(self):
+        self.pi.set_PWM_dutycycle(self.R, self.OFF)
+
+    def green_off(self):
+        self.pi.set_PWM_dutycycle(self.G, self.OFF)
+
+    def blue_off(self):
+        self.pi.set_PWM_dutycycle(self.B, self.OFF)
+
+    def all_on(self):
+        self.pi.set_PWM_dutycycle(self.R, self.ON)
+        self.pi.set_PWM_dutycycle(self.G, self.ON)
+        self.pi.set_PWM_dutycycle(self.B, self.ON)
+
+    def all_off(self):
+        self.pi.set_PWM_dutycycle(self.R, self.OFF)
+        self.pi.set_PWM_dutycycle(self.G, self.OFF)
+        self.pi.set_PWM_dutycycle(self.B, self.OFF)
+
+    def stop(self):
+        """
+        Free GPIO PINS 
+        """
+        self.pi.stop()   
