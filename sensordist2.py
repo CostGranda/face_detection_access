f49b51bca17a4579eb6018e258ddfe12be23a9ea
JL Granda <jorgelugra@MacBook-Pro-de-JL.local>
2019-05-14 11:09:58 -0500

    distanec class

--- sensordist2.py
+++ sensordist2.py
@@ -1,39 +1,57 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 try:
-    import RPi.GPIO as GPIO    #Importamos la librería GPIO
+    import RPi.GPIO as GPIO  # Importamos la librería GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!")
-import time                #Importamos time (time.sleep)
+import time  # Importamos time (time.sleep)
 from rekognition_apis import RekognitionApis
-from simple_storage_service import SimpleStorageServicce 
+from simple_storage_service import SimpleStorageServicce
 from get_frame import CaptureFrame
 
-GPIO.setmode(GPIO.BCM)     #Ponemos la placa en modo BCM
-GPIO_TRIGGER = 20          #Usamos el pin GPIO 25 como TRIGGER
-GPIO_ECHO    = 21           #Usamos el pin GPIO 7 como ECHO
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  #Configuramos Trigger como salida
-GPIO.setup(GPIO_ECHO,GPIO.IN)      #Configuramos Echo como entrada
-GPIO.output(GPIO_TRIGGER,False)    #Ponemos el pin 25 como LOW
+class DistanceSensor():
+    def __init__(self, trigg_pin, echo_pin):
+        # Usamos el pin GPIO como TRIGGER
+        self.GPIO_TRIGGER = trigg_pin
+        # Usamos el pin GPIO como ECHO
+        self.GPIO_ECHO = echo_pin
 
+    def setup(self, parameter_list):
+        # Ponemos la placa en modo BCM
+        GPIO.setmode(GPIO.BCM)
+        # Configuramos Trigger como salida
+        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+        # Configuramos Echo como entrada
+        GPIO.setup(self.GPIO_ECHO, GPIO.IN)  
+        # Ponemos el pin 25 como LOW
+        GPIO.output(self.GPIO_TRIGGER, False)  
+    
+    def get_distance(self):
+        GPIO.output(self.GPIO_TRIGGER, True)  # Enviamos un pulso de ultrasonidos
+        time.sleep(0.00001)  # Una pequeñña pausa
+        GPIO.output(self.GPIO_TRIGGER, False)  # Apagamos el pulso
+        start = time.time()  # Guarda el tiempo actual mediante time.time()
+        # Mientras el sensor no reciba señal...
+        while GPIO.input(self.GPIO_ECHO) == 0:
+            start = time.time()  # Mantenemos el tiempo actual mediante time.time()
+        while GPIO.input(self.GPIO_ECHO) == 1:  # Si el sensor recibe señal...
+            stop = time.time()  # Guarda el tiempo actual mediante time.time() en otra variable
+        elapsed = stop-start  # Obtenemos el tiempo transcurrido entre envío y recepción
+        # Distancia es igual a tiempo por velocidad partido por 2   D = (T x V)/2
+        distance = (elapsed * 34300)/2
+        return distance
+
+lo_distanceSnsr = DistanceSensor(20, 21)
 
 try:
-    while True:     #Iniciamos un loop infinito
-        GPIO.output(GPIO_TRIGGER,True)   #Enviamos un pulso de ultrasonidos
-        time.sleep(0.00001)              #Una pequeñña pausa
-        GPIO.output(GPIO_TRIGGER,False)  #Apagamos el pulso
-        start = time.time()              #Guarda el tiempo actual mediante time.time()
-        while GPIO.input(GPIO_ECHO)==0:  #Mientras el sensor no reciba señal...
-            start = time.time()          #Mantenemos el tiempo actual mediante time.time()
-        while GPIO.input(GPIO_ECHO)==1:  #Si el sensor recibe señal...
-            stop = time.time()           #Guarda el tiempo actual mediante time.time() en otra variable
-        elapsed = stop-start             #Obtenemos el tiempo transcurrido entre envío y recepción
-        distance = (elapsed * 34300)/2   #Distancia es igual a tiempo por velocidad partido por 2   D = (T x V)/2
-        if distance <=100:
+    while True:  # Iniciamos un loop infinito
+        distance = lo_distanceSnsr.get_distance()
+        if distance <= 100:
             print("LLAMADO AL METODO")
             time.sleep(10)
-        print(distance)                  #Devolvemos la distancia (en centímetros) por pantalla
-        time.sleep(1)                    #Pequeña pausa para no saturar el procesador de la Raspberry
-except KeyboardInterrupt:                #Si el usuario pulsa CONTROL+C...
-    print("quit")                        #Avisamos del cierre al usuario
-    GPIO.cleanup()                       #Limpiamos los pines GPIO y salimos
+        print(distance)  # Devolvemos la distancia (en centímetros) por pantalla
+        # Pequeña pausa para no saturar el procesador de la Raspberry
+        time.sleep(1)
+except KeyboardInterrupt:  # Si el usuario pulsa CONTROL+C...
+    print("quit")  # Avisamos del cierre al usuario
+    GPIO.cleanup()  # Limpiamos los pines GPIO y salimos
